20785fadd5fdf08830d90719c9a58e44ca308c7a
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2023-08-27 21:24:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
+
+  subscription_id = a7722c19-04cb-44e4-97d9-1829e898c977
+  tenant_id       = fa97aabc-9b54-4b0d-afa8-bdabd8afb40b
+  client_id       = var.dev_client_id
+  client_secret   = var.dev_client_secret
+
   features {}
 }
 
