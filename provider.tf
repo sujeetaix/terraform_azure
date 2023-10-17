5b3b1a9872e42f1cb4867c9b2cda760614408de5
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2023-10-17 20:06:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,8 +11,8 @@ provider "azurerm" {
 
   subscription_id = a7722c19-04cb-44e4-97d9-1829e898c977
   tenant_id       = fa97aabc-9b54-4b0d-afa8-bdabd8afb40b
-  client_id       = f89358b8-5376-4ab2-a4e6-543427e42f39
-  client_secret   = 2c2d13d7-666c-41ac-8359-08d7520748e3
+  client_id       = 71bfd28a-3b38-46d0-9f40-5b50e82a5539
+  client_secret   = gRt8Q~2c_G1d1Eero9matX33Is7UwC5GLq.5aaqn
 
   features {}
 }
