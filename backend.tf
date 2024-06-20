598b9926051995df982ec4dfc46bb0cd2d8d32bc
sujeetaix <77736892+sujeetaix@users.noreply.github.com>
2024-06-20 23:44:06 +1000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform"
-    storage_account_name = "terraform761"
+    storage_account_name = "terraform760"
     container_name       = "tfstate"
     key                  = "default.terraform.tfstate"
   }
